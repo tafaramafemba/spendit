762966602efdf311bc614106c26784a7627b0df9
tafaramafemba <tafara.maf@gmail.com>
2022-08-04 02:16:21 +0200

    ammend production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "spendit_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -93,4 +92,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://boiling-eyrie-21926.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'boiling-eyrie-21926.herokuapp.com',
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name => 'tafara.maf@gmail.com',
+    :password => 'rutendoma1'
+  }
 end
