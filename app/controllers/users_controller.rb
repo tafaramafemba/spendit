13363719cf5064c8fa1b22912e1110a8ff2ac845
tafaramafemba <tafara.maf@gmail.com>
2022-08-04 11:45:57 +0200

    amend sign out variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show; end
 
   def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    signed_out?(Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     redirect_to root_path
   end
 end
