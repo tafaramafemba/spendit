2a23d184630e579545f29feee5659cfe908b2410
tafaramafemba <tafara.maf@gmail.com>
2022-08-04 02:05:30 +0200

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "spendit_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -90,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://boiling-eyrie-21926.herokuapp.com/' }
 end
