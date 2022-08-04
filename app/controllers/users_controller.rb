4bd4bbd0e9d65890de7220f831c8efd6bc141614
tafaramafemba <tafara.maf@gmail.com>
2022-08-04 09:58:44 +0200

    set destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show; end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    redirect_to root_path
+  end
 end
